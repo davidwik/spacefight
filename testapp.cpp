3f56986d4a73e3166813edaced2f608e70b1445a
David Wiktorsson <david.wiktorsson@gmail.com>
2014-05-19 19:24:05 +0200

    Using iterators with map and vector to clean up resources.

--- testapp.cpp
+++ testapp.cpp
@@ -1,6 +1,7 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <vector>
+#include <map>
 #include <string>
 #include "utils.h"
 
@@ -9,6 +10,7 @@ using namespace std;
 
 class Animation {
     vector <SDL_Surface *> imageList;
+    vector <SDL_Surface *>::iterator listIterator;
     int fps;
     bool trans;
 public:
@@ -19,6 +21,7 @@ public:
     Animation(int framesPerSecond, bool transparent);
     ~Animation();
 };
+
 
 
 Animation::Animation(int framesPerSecond, bool transparent = false){
@@ -39,8 +42,17 @@ SDL_Surface* Animation::getFrame(unsigned int i){
 }
 
 Animation::~Animation(){
-    printf("Deleted");
+    // Free resources??
+    printf("Killing all images!\n");
+    for(listIterator = imageList.begin();
+        listIterator != imageList.end();
+        listIterator++){
+        SDL_FreeSurface(*listIterator);
+    }
 }
+
+map <string, Animation*> animationList;
+map <string, Animation*>::iterator mapIter;
 
 void Animation::loadImageIntoList(string imageURL){
     printf("Loading: %s", imageURL.c_str());
@@ -59,27 +71,53 @@ int main (int argc, char* argv[]){
         return 1;
     }
 
+    animationList["WALK"] = new Animation(4);
+    animationList["WALK"]->loadImageIntoList("gfx/1.jpg");
+    animationList["WALK"]->loadImageIntoList("gfx/2.jpg");
+    animationList["WALK"]->loadImageIntoList("gfx/3.jpg");
+    animationList["WALK"]->loadImageIntoList("gfx/4.jpg");
+    animationList["WALK"]->loadImageIntoList("gfx/5.jpg");
+
+    animationList["RUN"] = new Animation(4);
+    animationList["RUN"]->loadImageIntoList("gfx/1.jpg");
+    animationList["RUN"]->loadImageIntoList("gfx/2.jpg");
+    animationList["RUN"]->loadImageIntoList("gfx/3.jpg");
+    animationList["RUN"]->loadImageIntoList("gfx/4.jpg");
+    animationList["RUN"]->loadImageIntoList("gfx/5.jpg");
 
 
-   Animation anim = Animation(2, false);
-   anim.loadImageIntoList("gfx/1.jpg");
-   anim.loadImageIntoList("gfx/2.jpg");
-   anim.loadImageIntoList("gfx/3.jpg");
-   anim.loadImageIntoList("gfx/4.jpg");
-   anim.loadImageIntoList("gfx/5.jpg");
 
-   applySurface(0, 10, anim.getFrame(0), screen, NULL);
-   applySurface(100, 10, anim.getFrame(1), screen, NULL);
-   applySurface(200, 10, anim.getFrame(2), screen, NULL);
-   applySurface(300, 10, anim.getFrame(3), screen, NULL);
-   applySurface(400, 10, anim.getFrame(4), screen, NULL);
-   applySurface(500, 10, anim.getFrame(5), screen, NULL);
-   if(SDL_Flip(screen) == -1){
-       printf("Error flipping\n");
-       return 1;
-   }
+    applySurface(0, 10, animationList["WALK"]->getFrame(0), screen, NULL);
+    applySurface(100, 10, animationList["WALK"]->getFrame(1), screen, NULL);
+    applySurface(200, 10, animationList["WALK"]->getFrame(2), screen, NULL);
+    applySurface(300, 10, animationList["WALK"]->getFrame(3), screen, NULL);
+    applySurface(400, 10, animationList["WALK"]->getFrame(4), screen, NULL);
 
-   SDL_Delay(3000);
-   SDL_Quit();
-   return 0;
+    applySurface(0, 120, animationList["RUN"]->getFrame(0), screen, NULL);
+    applySurface(100, 120, animationList["RUN"]->getFrame(1), screen, NULL);
+    applySurface(200, 120, animationList["RUN"]->getFrame(2), screen, NULL);
+    applySurface(300, 120, animationList["RUN"]->getFrame(3), screen, NULL);
+    applySurface(400, 120, animationList["RUN"]->getFrame(4), screen, NULL);
+
+
+    if(SDL_Flip(screen) == -1){
+        printf("Error flipping\n");
+        return 1;
+    }
+
+
+
+    SDL_Delay(3000);
+
+    for(std::map<string, Animation*>::iterator mapIter=animationList.begin();
+        mapIter!=animationList.end();
+        mapIter++){
+        delete mapIter->second;
+    }
+
+    //delete animationList["WALK"];
+    //delete animationList["RUN"];
+
+    SDL_Quit();
+    return 0;
 }
